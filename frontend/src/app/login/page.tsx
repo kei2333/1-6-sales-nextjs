b68f476fdd02e1767b9b8494617a9ac744b1868f
Kohki <140950795+kohki-s@users.noreply.github.com>
2025-05-02 17:08:13 +0900

    Inserted link for sending email

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -6,7 +6,6 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#BAC43E]/40 space-y-4">
-      {/* タイトル */}
       <h1 className="text-2xl font-bold text-gray-800">売上管理おまかせくん</h1>
 
       <div className="w-full max-w-sm bg-white p-8 rounded shadow-md space-y-6">
@@ -20,7 +19,13 @@ export default function Home() {
           </Button>
 
           <p className="mt-4 text-xs text-gray-600 text-center">
-            ログインできない方は管理者にお問い合わせください。
+            ログインできない方は{" "}
+            <a
+              href="mailto:kohkishibata@aaa.com"
+              className="text-blue-600 underline hover:text-blue-800"
+            >
+              管理者にお問い合わせください。
+            </a>
           </p>
         </div>
       </div>
