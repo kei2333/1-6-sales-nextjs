8d2826b35ee6a6b1c7c50c634fd5e1f3b08e0216
Kohki <140950795+kohki-s@users.noreply.github.com>
2025-05-07 16:34:27 +0900

    edited function_app.py

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,6 +1,6 @@
-import type {  NextAuthOptions  } from "next-auth"
 import AzureADProvider from "next-auth/providers/azure-ad";
 import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 
 export const authOptions = {
     providers: [
@@ -9,20 +9,7 @@ export const authOptions = {
             clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
             tenantId: process.env.AZURE_AD_TENANT_ID!,
          }),
-  ]//,
-  // callbacks: {
-  //   async signIn({ user }) {
-  //     // 許可するユーザーのメールアドレス一覧
-  //     const allowedEmails = ['TCS2025_TP_class1_27@uhd.onmicrosoft.com', 'user2@example.com'];
-
-  //     if (allowedEmails.includes(user.email)) {
-  //       return true; // 許可
-  //     }
-
-  //     console.warn(`Unauthorized login attempt by ${user.email}`);
-  //     return false; // 拒否
-  //   },
-  // },
+  ]
 } satisfies NextAuthOptions
 
-export const {  handlers, auth, signIn, signOut  } = NextAuth(authOptions)
+export const { handlers, auth, signIn, signOut } = NextAuth(authOptions)
