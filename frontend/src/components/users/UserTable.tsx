f52cb206812de6f7d8f407d64493036d6eaabccd
SakiYoshida1 <sakitty22202@gmail.com>
2025-05-08 10:31:57 +0900

    feat: Employee, User型定義を追加しました吉田

--- frontend/src/components/users/UserTable.tsx
+++ frontend/src/components/users/UserTable.tsx
@@ -13,14 +13,29 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { useEffect } from "react";
 
+type Employee = {
+  employee_number: number;
+  employee_name: string;
+  employee_role: string;
+  location_id: number;
+};
+type User = {
+  id: number;
+  name: string;
+  role: string;
+  email: string;
+  updatedAt: string;
+};
+
+
 
 
 const roleOptions = ["Sales", "IT", "Manager","権限なし"];
 
 export default function UserTable() {
   const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [, setLoading] = useState(false);
+  const [, setError] = useState("");
   const [editingId, setEditingId] = useState<number | null>(null);
   const [editedName, setEditedName] = useState("");
   const [editedRole, setEditedRole] = useState("");
