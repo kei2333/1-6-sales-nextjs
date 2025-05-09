08218513da741acda1bf210ba0bd1b081c6067b5
Kohki <140950795+kohki-s@users.noreply.github.com>
2025-05-09 21:10:08 +0900

    e

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -1,18 +1,16 @@
-'use client';
+// app/admin/users/page.tsx
+"use client";
 
-import { useSession } from "next-auth/react";
+import UserSearchBar from "@/components/users/UserSearchBar";
+import UserTable from "@/components/users/UserTable";
 
 export default function UserManagementPage() {
-  const { data: session, status } = useSession();
-
-  console.log("UserManagementPage status:", status);
-  console.log("UserManagementPage session:", session);
-
   return (
     <>
       <div className="flex items-center justify-between mb-4">
-        <p>User Management Page Loaded</p>
+        <UserSearchBar />
       </div>
+      <UserTable location_id={0}/>
     </>
   );
 }
